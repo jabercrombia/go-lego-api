a1695b72f4fb990ee6cfeb8bd9e82c463ef6a28f
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-05-02 14:59:05 -0500

    tracking

--- lego-app/app/layout.tsx
+++ lego-app/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "../styles/headings.scss"
 import Footer from "../components/footer"
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 import Header from "../components/header"
 import { Lato } from 'next/font/google'
 
@@ -12,8 +14,27 @@ const lato = Lato({
  
 
 export const metadata: Metadata = {
-  title: "Lego Set API",
-  description: "Created using Nextjs and Go",
+  title: `Lego Set API`,
+  description: `Discover a powerful LEGO API that provides detailed information about LEGO sets, minifigures, parts, themes, and more. Perfect for developers, hobbyists, and collectors looking to build LEGO-powered applications with ease and accuracy.`,
+  keywords :[
+    "LEGO API",
+    "LEGO set data",
+    "LEGO minifigures API",
+    "LEGO parts database",
+    "LEGO themes",
+    "LEGO for developers",
+    "LEGO collection API",
+    "LEGO JSON API",
+    "LEGO programming",
+    "LEGO data integration",
+    "LEGO hobbyist tools",
+    "LEGO collectors app"
+  ],
+  authors: [{ name: 'Justin Abercrombia', url: 'http://www.github.com/jabercrombia' }],
+  creator: 'Justin Abercrombia',
+  openGraph: {
+    images: '/homepage.png',
+  },
 };
 
 export default function RootLayout({
@@ -32,6 +53,7 @@ export default function RootLayout({
           <Footer/>
         </div>
       </body>
+      <GoogleAnalytics gaId={process.env.GOOGLE_TRACKIND_ID || ''} />
     </html>
   );
 }
