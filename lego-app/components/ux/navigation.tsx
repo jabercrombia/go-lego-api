8a4451a93554569c551ed711640dcb399e12d9d5
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-05-04 21:04:16 -0500

    nav

--- lego-app/components/ux/navigation.tsx
+++ lego-app/components/ux/navigation.tsx
@@ -1,77 +1,129 @@
-// app/page.tsx or pages/index.tsx
-'use client';
+"use client"
 
-import "../../styles/nav.scss"
 import * as React from "react"
 import Link from "next/link"
-
+import "../../styles/nav.scss"
+import { cn } from "@/lib/utils"
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    navigationMenuTriggerStyle,
-  } from "@/components/ui/navigation-menu"
-  
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
 
-// const navigation = [
-//     {
-//         'name': 'home',
-//         'path': '/'
-//     },
-//     {
-//         'name': 'docs',
-//         'path': '/docs'
-//     },
-//     {
-//         'name': 'endpoints',
-//         'path': '/api/allsets'
-//     },
-// ];
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "All Lego Sets",
+    href: "/api/allsets",
+    description:
+      "Retrieves a list of all LEGO sets, including set names, numbers, themes, release years, and piece counts.",
+  },
+  {
+    title: "Specific Sets",
+    href: "/api/sets/1",
+    description:
+      "Returns detailed information about a specific LEGO set, including its name, number, theme, year, minifigures, and parts, based on the provided set ID.",
+  }
+]
 
-
-
-export default function HomePage() {
+export default function NavigationMenuDemo() {
   return (
-    // <nav className="flex flex-row gap-2">
-    //     <div className="flex items-center">
-    //         <div className="h-[20px] w-[20px] bg-yellow-300 rounded-md"></div>
-    //         <div className="w-[100px]">
-    //             <Link href="/">
-    //                 Lego Set API
-    //             </Link>
-    //         </div>
-    //     </div>
-
-    //     <div className="flex pl-20 gap-7">
-    //         {
-    //             navigation.map((elem, index) =>{
-    //                 return (
-    //                     <div key={index}>
-    //                         <Link href={elem.path}>{elem.name}</Link>
-    //                     </div>
-    //                 )
-    //             })
-    //         }
-    //     </div>
-    // </nav>
     <NavigationMenu>
-  <NavigationMenuList>
-    <NavigationMenuItem>
-      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <NavigationMenuLink>Link</NavigationMenuLink>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-    <Link href="/docs" legacyBehavior passHref>
-    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-      Documentation
-    </NavigationMenuLink>
-  </Link>
-  </NavigationMenuList>
-</NavigationMenu>
-
-  );
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Home
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      shadcn/ui
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Beautifully designed components that you can copy and
+                      paste into your apps. Accessible. Customizable. Open
+                      Source.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </ListItem>
+              <ListItem href="/docs/installation" title="Installation">
+                How to install dependencies and structure your app.
+              </ListItem>
+              <ListItem href="/docs/primitives/typography" title="Typography">
+                Styles for headings, paragraphs, lists...etc
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
