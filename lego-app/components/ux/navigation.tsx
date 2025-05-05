1c2f2d6da29395dd2f030a94102a52bb27e8cd73
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-05-04 20:36:41 -0500

    nav

--- lego-app/components/ux/navigation.tsx
+++ lego-app/components/ux/navigation.tsx
@@ -5,6 +5,19 @@ import "../../styles/nav.scss"
 import * as React from "react"
 import Link from "next/link"
 
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+    navigationMenuTriggerStyle,
+  } from "@/components/ui/navigation-menu"
+  
+
 const navigation = [
     {
         'name': 'home',
@@ -24,27 +37,43 @@ const navigation = [
 
 export default function HomePage() {
   return (
-    <nav className="flex flex-row gap-2">
-        <div className="flex items-center">
-            <div className="h-[20px] w-[20px] bg-yellow-300 rounded-md"></div>
-            <div className="w-[100px]">
-                <Link href="/">
-                    Lego Set API
-                </Link>
-            </div>
-        </div>
+    // <nav className="flex flex-row gap-2">
+    //     <div className="flex items-center">
+    //         <div className="h-[20px] w-[20px] bg-yellow-300 rounded-md"></div>
+    //         <div className="w-[100px]">
+    //             <Link href="/">
+    //                 Lego Set API
+    //             </Link>
+    //         </div>
+    //     </div>
 
-        <div className="flex pl-20 gap-7">
-            {
-                navigation.map((elem, index) =>{
-                    return (
-                        <div key={index}>
-                            <Link href={elem.path}>{elem.name}</Link>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    </nav>
+    //     <div className="flex pl-20 gap-7">
+    //         {
+    //             navigation.map((elem, index) =>{
+    //                 return (
+    //                     <div key={index}>
+    //                         <Link href={elem.path}>{elem.name}</Link>
+    //                     </div>
+    //                 )
+    //             })
+    //         }
+    //     </div>
+    // </nav>
+    <NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink>Link</NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+    <Link href="/docs" legacyBehavior passHref>
+    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+      Documentation
+    </NavigationMenuLink>
+  </Link>
+  </NavigationMenuList>
+</NavigationMenu>
+
   );
 }
